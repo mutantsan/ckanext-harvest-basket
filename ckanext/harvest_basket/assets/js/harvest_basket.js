ce330a09091a74f73ad9ff4f0ebfb76a8b552623
mutantsan <kvaqich@gmail.com>
2022-02-18 12:25:29 +0200

    wrote some basic css and js, include assets

---
+++ ckanext/harvest_basket/assets/js/harvest_basket.js
@@ -0,0 +1,59 @@
+ckan.module("harvest-change-source-type", function ($, _) {
+    "use strict";
+    return {
+        options: {},
+
+        initialize: function () {
+            this.client = this.sandbox.client;
+            this.preview = ".harvest-checkup-preview";
+            this._check_source(this._get_selected_type());
+
+            $(".harvest-types input").change(function (e) {
+                this._check_source(e.target.value)
+            }.bind(this))
+        },
+
+        _check_source: function (source_name) {
+            var that = this;
+
+            $(that.preview).removeClass("new error");
+            this._add_pending_state();
+
+            that.client.call('POST', 'harvest_basket_check_source', {
+                source_name: source_name
+            }, function (response) {
+                that._show_check_result(response.result)
+                that._remove_pending_state();
+            }, function (response) {
+                var err_msg = response.responseJSON.error.message
+                console.warn(err_msg);
+                that._show_check_result(err_msg)
+                $(that.preview).toggleClass("error");
+                that._remove_pending_state();
+            });
+
+        },
+
+        _show_check_result: function (check_result) {
+            var that = this;
+
+            $(that.preview + " .preview-field").text(
+                $.type(check_result) == "string" ? check_result : JSON.stringify(check_result)
+            )
+
+            $(that.preview).toggleClass("new");
+        },
+
+        _get_selected_type: function () {
+            return $(".harvest-types input:checked").val()
+        },
+
+        _add_pending_state: function () {
+            $(".harvest-types").addClass("pending");
+        },
+
+        _remove_pending_state: function () {
+            $(".harvest-types").removeClass("pending")
+        }
+    };
+});
