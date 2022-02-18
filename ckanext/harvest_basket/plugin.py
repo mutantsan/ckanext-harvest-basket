18e434ef289044a81e8eaf1549d4018ef5a54f0c
mutantsan <kvaqich@gmail.com>
2022-02-18 12:20:35 +0200

    register action and auth, remove old scripts/styles

--- ckanext/harvest_basket/plugin.py
+++ ckanext/harvest_basket/plugin.py
@@ -1,9 +1,14 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.harvest_basket.action.logic import get_actions
+from ckanext.harvest_basket.action.auth import get_auth_functions
+
 
 class HarvestBasketPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IAuthFunctions)
 
     # IConfigurer
 
@@ -11,3 +16,13 @@ class HarvestBasketPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "harvest_basket")
+
+    # IActions
+
+    def get_actions(self):
+        return get_actions()
+
+    # IAuthFunctions
+
+    def get_auth_functions(self):
+        return get_auth_functions()
