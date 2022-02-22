3614769b4c31bb4535b044d73433789266a3dfee
mutantsan <kvaqich@gmail.com>
2022-02-22 16:26:34 +0200

    changes into source_check action

--- ckanext/harvest_basket/action/logic/get.py
+++ ckanext/harvest_basket/action/logic/get.py
@@ -8,6 +8,7 @@ import ckan.plugins.toolkit as tk
 
 from ckanext.harvest_basket.harvesters import DKANHarvester
 from ckanext.harvest_basket.harvesters import JunarHarvester
+from ckanext.harvest_basket.harvesters import SocrataHarvester
 
 
 @tk.side_effect_free
@@ -16,14 +17,15 @@ def check_source(ctx: dict[str, Any], data_dict: dict) -> dict[str, Any]:
 
     source_name: str = tk.get_or_bust(data_dict, "source_name")
     source_url: Optional[str] = data_dict.get("source_url")
-    config: dict = json.loads(data_dict.get("config", "{}"))
+    config: dict = json.loads(data_dict.get("config") or "{}")
     
     if not source_url:
         raise tk.ValidationError(f"The source URL must be provided to make a checkup")
 
     sources = {
         "dkan": DKANHarvester,
-        "junar": JunarHarvester
+        "junar": JunarHarvester,
+        "socrata": SocrataHarvester
     }
 
     harvester_class = sources.get(source_name)
@@ -31,4 +33,4 @@ def check_source(ctx: dict[str, Any], data_dict: dict) -> dict[str, Any]:
     if not harvester_class:
         raise tk.ValidationError(f"The source checkup for type `{source_name}` not implemented")
     
-    return harvester_class().make_checkup(source_url, config)
+    return harvester_class().make_checkup(source_url, source_name, config)
