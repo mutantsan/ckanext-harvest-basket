5cb03070c1423875f373db256d0caf44263a7771
mutantsan <kvaqich@gmail.com>
2022-03-07 15:18:23 +0200

    add option to restrict harvester pages for anon users

--- ckanext/harvest_basket/action/auth/__init__.py
+++ ckanext/harvest_basket/action/auth/__init__.py
@@ -1,7 +1,9 @@
 from . import get
+from .get import package_search
 
 
 def get_auth_functions():
     return {
         "harvest_basket_check_source": get.check_source,
+        "package_search": package_search,
     }
--- ckanext/harvest_basket/action/auth/get.py
+++ ckanext/harvest_basket/action/auth/get.py
@@ -1,3 +1,16 @@
+from ckan.plugins import toolkit as tk
+
+
 def check_source(ctx, data_dict):
     # sysadmins only
     return {"success": False}
+
+@tk.auth_allow_anonymous_access
+@tk.chained_auth_function
+def package_search(next_, context, data_dict):
+    view, action = tk.get_endpoint()
+    allow_anon: bool = tk.asbool(tk.config.get("ckanext.harvest_basket.allow_anonymous", 1))
+
+    if not allow_anon and view == "harvest" and not any((tk.g.user, tk.g.userobj)):
+        return tk.abort(403, tk._('Anonymous user cannot see this page'))
+    return next_(context, data_dict)
